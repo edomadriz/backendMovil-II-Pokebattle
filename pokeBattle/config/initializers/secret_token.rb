54446b1e9f29f8bab6708393ea646521115fcb17
Gabriel <edomadriz@gmail.com>
2014-11-09 18:09:31 -0600

    Base project

---
+++ pokeBattle/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PokeBattle::Application.config.secret_token = '572a2bba787416051294b7e916e2d532cf5381e5f835ba91f5d84c008adecf2ac37251945f4cb670fa819ada78f84a086d06290907842755ca57db2e7aac93aa'
